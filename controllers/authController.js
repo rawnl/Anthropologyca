a98ff3cd8975de8b7948d9df6857a009a3264c04
Raw NL <www.angyangelina654@gmail.com>
2023-08-04 23:33:43 +0200

    added Logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,3 +68,14 @@ exports.login = catchAsync(async (req, res, next) => {
 
   createAndSendToken(user, 200, res);
 });
+
+exports.logout = (req, res) => {
+  res.cookie('jwt', '', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+  });
+};
