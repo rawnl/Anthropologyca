9345d32c9b3f6c9bbd75fbae0e5125fa3d324cd2
Raw NL <www.angyangelina654@gmail.com>
2023-08-26 15:07:32 +0200

    Error handling

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
 const bodyParser = require('body-parser');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
 const commentRouter = require('./routes/commentRoutes');
 const likeRouter = require('./routes/likeRoutes');
 
 const app = express();
+
+// Enable cors
+app.use(cors());
+app.options('*', cors());
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Set security HTTP Headers
+app.use(helmet({ contentSecurityPolicy: false }));
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same IP address
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 5 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later',
+});
+
+app.use('/api', limiter);
 
 // Body parser
 app.use(
@@ -14,10 +51,41 @@ app.use(
   })
 );
 
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10kb',
+  })
+);
+
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution ==> To be defined
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+app.use(compression());
+
 // Routes
-app.use('/users', userRouter);
-app.use('/posts', postRouter);
-app.use('/comments', commentRouter);
-app.use('/likes', likeRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/likes', likeRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
